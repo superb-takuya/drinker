ce121b9dba9cf2cf2e11fc61d5533a85897e2ea4
takuya911 <takuya.engineer911@gmail.com>
2020-12-05 12:11:23 +0900

    header foote作成

--- front/components/common/header.vue
+++ front/components/common/header.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    top page
+    header
   </div>
 </template>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <app-header />
     <Nuxt />
+    <app-footer />
   </div>
 </template>
+
+
+<script>
+import AppHeader from "~/components/common/header.vue"
+import AppFooter from "~/components/common/footer.vue"
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+}
+</script>

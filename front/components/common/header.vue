d817dddf1bcbc52be1f7561b5c76add05ded0068
takuya911 <takuya.engineer911@gmail.com>
2020-12-06 15:54:54 +0900

    header

--- front/components/common/header.vue
+++ front/components/common/header.vue
@@ -1,15 +1,110 @@
 <template>
-  <el-header>
+  <el-header class="site-header">
     <el-container>
-      <div class="header-right">
+      <div class="header-left flex">
         <nuxt-link to="/">
-        <el-image class="site-image" src="/images/title.png" fit="contain"></el-image>
-      </nuxt-link>
+          <el-image class="site-image" src="/images/title.png" fit="contain"></el-image>
+        </nuxt-link>
+        <div class="header-left-links flex">
+          <div><nuxt-link to="/">ホーム</nuxt-link></div>
+          <div><nuxt-link to="/chat">メッセージ</nuxt-link></div>
+          <div><nuxt-link to="/cast/1">プロフィール</nuxt-link></div>
+        </div>
       </div>
-      <div class="header-left">
-        <el-button round plain>ログイン</el-button>
-        <el-button round type="primary">新規登録</el-button>
+      <div class="header-right">
+        <div class="after-auth" v-if="auth">
+          <nuxt-link to="/notice">
+            <i class="icon el-icon-bell"></i>
+          </nuxt-link>
+          <nuxt-link to="/chat">
+            <i class="icon el-icon-chat-dot-round"></i>
+          </nuxt-link>
+          <i class="icon el-icon-menu drawer-small" @click="drawer = true"></i>
+        </div>
+        <div class="before-auth" v-else>
+          <el-button round plain>ログイン</el-button>
+          <el-button round type="primary">新規登録</el-button>
+        </div>
       </div>
     </el-container>
+
+    <!-- drawer -->
+    <el-drawer :visible.sync="drawer" :with-header="false" id="header-drawer" size="25%">
+      <el-row>
+        <el-col>
+          <el-header class="drawer-header">
+          <div class="user flex">
+            <div class="icon">
+              <el-avatar class="icon" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+            </div>
+            <div>
+              <div class="username">なまえ</div>
+              <div class="credit">所有クレジット：500</div>
+            </div>
+          </div>
+          </el-header>
+          <el-menu>
+            <el-menu-item v-for="i in drawerData" :key="i.title" @click="showPage(i.link)">
+              <span>{{i.title}}</span>
+            </el-menu-item>
+            <el-menu-item @click="signOut">
+              <span>ログアウト</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-drawer>
   </el-header>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      auth: true,
+      drawer: false,
+      drawerData:[
+        {
+          title: 'プロフィール',
+          link: '/cast/1',
+        },
+        {
+          title: 'プロフィール編集',
+          link: '/mypage/edit',
+        },
+        {
+          title: 'アカウント設定',
+          link: '/mypage/setting',
+        },
+        {
+          title: 'クレジット購入/履歴',
+          link: '/credit/market',
+        },
+        {
+          title: 'ダッシュボード',
+          link: '/dashbord/ticket',
+        },
+      ]
+    };
+  },
+  mounted: function(){
+    // css
+    let smallMedia = window.matchMedia('(max-width: 480px)');
+    if(smallMedia.matches){
+      let drawer = document.getElementById('header-drawer');
+      drawer.getElementsByClassName('el-drawer')[0].style.width = "65%"
+    }
+  },
+  methods:{
+    showPage(pagelink){
+      this.drawer = false;
+      this.$router.push(pagelink);
+    },
+    signOut(){
+      // signOut
+      location.href="/";
+    }
+  }
+};
+</script>

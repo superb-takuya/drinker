69f8ff1997bbbb044480e3b80ba963663c1acf84
takuya911 <takuya.engineer911@gmail.com>
2020-12-06 12:24:24 +0900

    header before login

--- front/components/common/header.vue
+++ front/components/common/header.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    header
-  </div>
+  <el-header>
+    <el-container>
+      <div class="header-right">
+        <nuxt-link to="/">
+        <el-image class="site-image" src="/images/title.png" fit="contain"></el-image>
+      </nuxt-link>
+      </div>
+      <div class="header-left">
+        <el-button round plain>ログイン</el-button>
+        <el-button round type="primary">新規登録</el-button>
+      </div>
+    </el-container>
+  </el-header>
 </template>

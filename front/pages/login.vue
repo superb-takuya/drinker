9af265354e6927611af0fead4fc6c6dbb1e16028
takuya911 <takuya.engineer911@gmail.com>
2020-12-12 14:12:27 +0900

    login page

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>
-    login
+  <div class="login">
+    <div class="container pt-1">
+      <el-row type="flex" class="row-bg" justify="center">
+        <el-col :xs="24" :sm="16" :md="8">
+          <el-card class="my-2 p-3" :body-style="{ padding: '0px' }">
+            <el-row type="flex" class="row-bg" justify="center">
+              <el-col :xs="24" :sm="24" :md="24">
+                <div class="title">Login!</div>
+              </el-col>
+            </el-row>
+            <el-row type="flex" class="row-bg" justify="center">
+              <el-col :xs="24" :sm="24" :md="24">
+                <el-form class="login-form">
+                  <el-form-item class="my-1">
+                    <el-input placeholder="メールアドレス" v-model="email"></el-input>
+                  </el-form-item>
+                  <el-form-item class="my-1">
+                    <el-input placeholder="パスワード" v-model="password"></el-input>
+                  </el-form-item>
+                  <el-form-item class="text-center mt-2">
+                    <el-button type="primary" >ログイン</el-button>
+                  </el-form-item>
+                </el-form>
+                <hr>
+                <div class="flex">
+                  <NuxtLink class="forget-password" to="/forget_password">パスワードを忘れた</NuxtLink>
+                  <NuxtLink class="register" to="/register">新規登録</NuxtLink>
+                </div>
+              </el-col>
+            </el-row>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        id:'1',
+        detail: 'クレジットの購入',
+        date:'2020-10-24',
+        credit: '+300',
+      },
+      {
+        id:`2`,
+        detail: 'A子さんとの飲み',
+        date:'2020-10-24',
+        credit: '-400',
+      }]
+    }
+  }
+}
+</script>
